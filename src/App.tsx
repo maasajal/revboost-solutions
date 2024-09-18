8ef662182909db05f000a2f97bd4c1829f5e4e07
rajiul93 <rajiulislam283@gmail.com>
2024-09-18 21:27:24 +0600

    select fort size font family p tag color select a fav icon select a background color

--- src/App.tsx
+++ src/App.tsx
@@ -7,15 +7,7 @@ const App = () => {
         <h1 className="  py-10 text-center">
           RevBoots Solutions
         </h1>
-        <h2 className=" ">
-          RevBoots Solutions
-        </h2>
-        <h3 className="  ">
-          RevBoots Solutions
-        </h3>
-        <h4 className="   ">
-          RevBoots Solutions
-        </h4>
+        
         <Home />
       </div>
     </>
