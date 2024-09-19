7b8480b611d14c0f9fccb1de517197e71bb3a54a
rfaraf <mohsinarfat53@gamil.com>
2024-09-19 13:41:13 +0600

    add banner

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,8 +1,10 @@
+import Banner from "./HomeComponent/Banner";
 import FAQ from "./HomeComponent/FAQ";
 
 const Home = () => {
   return (
     <div className="space-y-20">
+      <Banner></Banner>
       <FAQ />
     </div>
   );
