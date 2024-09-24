717145d3f379383ea752e55c9da92722c02f5d42
mohsinahmedarfat <mohsinarfat53@gamil.com>
2024-09-24 13:00:47 +0600

    modify: signup page

--- src/pages/authentication/register/Register.tsx
+++ src/pages/authentication/register/Register.tsx
@@ -5,18 +5,27 @@ import { FcGoogle } from "react-icons/fc";
 const Register = () => {
   return (
     <div className="min-h-screen flex items-center justify-center">
-      <div className="flex w-full  shadow-lg">
+      <div className="flex flex-col lg:flex-row w-full">
         {/* Left Section */}
-        <div className="flex-1 bg-[#FF08008C] flex flex-col items-center justify-center p-10">
-          <h1 className="text-5xl font-bold text-white mb-4">RevBoost</h1>
+        <div className="flex-1 bg-[#FF08008C] flex flex-col items-center pt-10 ">
+          <h1 className="text-5xl font-bold mb-4 bg-gradient-to-r from-[#ffffff] to-[#FF0000] inline-block text-transparent bg-clip-text">
+            RevBoost
+          </h1>
+          {/* <h1 className="text-5xl font-bold text-white mb-4">
+            Rev<span className="text-[#FF0000]">Boost</span>
+          </h1> */}
           <p className="text-lg text-white">
             A Modern Revenue Generation Platform
           </p>
-          <img src={moneyHome} alt="Money Home" className="mt-10 w-3/4" />
+          <img
+            src={moneyHome}
+            alt="Money Home"
+            className="w-3/4 lg:w-full mt-10 -mb-12 lg:mb-0 lg:ml-20 lg:-mr-40 z-10"
+          />
         </div>
 
         {/* Right Section (Form) */}
-        <div className="flex-1 bg-white p-10 flex flex-col justify-center">
+        <div className="flex-1 bg-white p-10 lg:pl-40 flex flex-col justify-center">
           <form className="w-full">
             <h2 className="text-3xl font-semibold text-center mb-6">
               Create Account
@@ -34,6 +43,7 @@ const Register = () => {
                   required
                 />
               </div>
+
               <div className="form-control">
                 <label className="label">
                   <span className="label-text">Last Name</span>
@@ -105,7 +115,7 @@ const Register = () => {
           </div>
 
           {/* Social Login */}
-          <div className="flex flex-col space-y-4">
+          <div className="flex flex-col md:flex-row lg:flex-col xl:flex-row justify-center gap-4">
             <button className="bg-white border border-gray-300 flex items-center justify-center gap-x-3 text-sm sm:text-base rounded-lg hover:bg-gray-100 duration-300 transition-colors px-8 py-2.5">
               <FcGoogle className="text-2xl" />
               <span>Sign up with Google</span>
