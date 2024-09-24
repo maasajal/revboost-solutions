7c86ca7a33c1fe49ad6de0e90e099441e04aa12b
mohsinahmedarfat <mohsinarfat53@gamil.com>
2024-09-24 17:08:57 +0600

    add: react hook form, toast, password verification, show password eye toggle

--- src/main.tsx
+++ src/main.tsx
@@ -5,11 +5,13 @@ import { Provider } from "react-redux";
 import { store } from "./app/store";
 import { RouterProvider } from "react-router-dom";
 import router from "./routes/Router";
+import { Toaster } from "react-hot-toast";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <Provider store={store}>
       <RouterProvider router={router} />
+      <Toaster />
     </Provider>
   </React.StrictMode>
 );
