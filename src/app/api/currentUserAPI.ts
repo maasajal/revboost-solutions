d82fab23849c6929f74b38153c17199b5e82a5b4
Abdullah Sajal <59488317+sajal-se@users.noreply.github.com>
2024-10-10 16:51:02 +0300

    Try to get currentUser by Email

---
+++ src/app/api/currentUserAPI.ts
@@ -0,0 +1,27 @@
+import { jwtDecode } from "jwt-decode";
+import {
+  fetchUsersStart,
+  fetchUsersFailure,
+  userRequestFailure,
+  fetchUserSuccess,
+} from "../features/users/currentUserSlice";
+
+import { axiosPublic } from "../hooks/useAxiosPublic";
+export interface DecodedToken {
+  email: string;
+}
+
+export const getCurrentUser = () => async (dispatch: any) => {
+  dispatch(fetchUsersStart());
+
+  const token = localStorage.getItem("user-token");
+  try {
+    if (token) {
+      const decoded = jwtDecode<DecodedToken>(token); // Decode the token with the defined type
+      const response = await axiosPublic.get(`/user/${decoded.email}`); // Assuming your API supports fetching by email
+      dispatch(fetchUserSuccess(response.data)); // Wrap in an array if your state expects an array
+    }
+  } catch (error: any) {
+    dispatch(userRequestFailure(error.message || "Failed to fetch user"));
+  }
+};
--- src/app/store/store.ts
+++ src/app/store/store.ts
@@ -4,6 +4,7 @@ import { persistReducer, persistStore } from "redux-persist";
 import storage from "redux-persist/lib/storage"; // স্থানীয় স্টোরেজ ব্যবহার করুন
 import authReducer from "../features/firebaseAuthentication/authSlice"; // আপনার authSlice এর সঠিক পাথ ব্যবহার করুন
 import usersReducer from "../features/users/usersSlice";
+import currentUserReducer from "../features/users/currentUserSlice";
 import revenueGrowthReducer from "../features/revenueGrowth/revenueGrowthSlice";
 import payrollReducer from "../state/payroll/payrollSlice";
 
@@ -19,6 +20,7 @@ export const store = configureStore({
   reducer: {
     auth: persistedAuthReducer, // authReducer যোগ করুন
     users: usersReducer, // get users data from the MongoDB userCollections
+    currentUser: currentUserReducer,
     revenueGrowth: revenueGrowthReducer,
     payroll: payrollReducer,
   },
