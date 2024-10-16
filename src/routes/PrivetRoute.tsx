0c59ee36e2b2bba54d69f84d6ba5722bf85cea3f
rajiul93 <rajiulislam283@gmail.com>
2024-10-16 20:58:56 +0600

    privet route create and apply complete

--- src/routes/PrivetRoute.tsx
+++ src/routes/PrivetRoute.tsx
@@ -1,6 +1,6 @@
 import { ReactNode } from "react";
 import { useSelector } from "react-redux";
-import { Navigate, useNavigate } from "react-router-dom";
+import { Navigate } from "react-router-dom";
 import { RootState } from "../app/store/store";
 
 interface PrivetRouteProps {
@@ -8,17 +8,12 @@ interface PrivetRouteProps {
 }
 
 const PrivetRoute = ({ children }: PrivetRouteProps) => {
-    const navigate = useNavigate()
-    const { user, loading } = useSelector((state: RootState) => state.auth);
+    const { user, loading } = useSelector((state: RootState) => state.auth); 
     if (loading) {
-        return <>Loading rajiul</>
-    }
-    if (!user) {
-        navigate("/login")
-        return
+        return <>Loading.....</>
     }
     if (user) return children
-    return <Navigate to='/login' state={location.pathname} replace={true} />
+    return <Navigate to='/login' state={location.pathname} replace />
 };
 
 export default PrivetRoute;
