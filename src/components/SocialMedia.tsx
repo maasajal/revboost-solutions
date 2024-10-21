7049101b642caf25ed1e7bc966202859467db49f
rajiul93 <rajiulislam283@gmail.com>
2024-10-22 00:54:46 +0600

    uptade contact page

---
+++ src/components/SocialMedia.tsx
@@ -0,0 +1,52 @@
+import { CiLinkedin, CiMail } from "react-icons/ci";
+import { FaInstagram } from "react-icons/fa";
+import { MdOutlinePhoneInTalk } from "react-icons/md";
+import { SlSocialFacebook } from "react-icons/sl";
+const SocialMedia = () => {
+  return (
+    <div className="space-y-3 px-6">
+      <h2>Contact Us</h2>
+      <p className=" text-sm font-montserrat">
+      <span className="font-bold">Consultation:</span> Once you fill out the "Contact Us" form, we will schedule a meeting to discuss your specific needs and gather more details about your inquiry.
+      </p>
+      <p className="text-sm  font-montserrat">
+      <span className="font-bold">Execution:</span> 
+       Upon your approval of the proposal, our team will begin executing the plan, providing regular updates and support throughout the process to ensure your satisfaction.
+      </p>
+      <div className="">
+        <div>
+          <p className="social-media">
+            <CiMail className=" text-xl" />
+            <a target="_blank" href="mailto:revbostsolution@gmail.com">
+            revboost@solutions.com
+            </a>
+          </p>
+          <p className="social-media">
+            <MdOutlinePhoneInTalk className="text-2xl" />
+            <a href="tel:+1234567890">+88 017 xxx xxx</a>
+          </p>
+          <p className="social-media">
+            <SlSocialFacebook className="text-2xl" />
+            <a target="_blank" href="https://facebook.com/">
+              Facebook
+            </a>
+          </p>
+          <p className="social-media">
+            <FaInstagram className=" text-2xl" />
+            <a target="_blank" href="https://www.instagram.com/">
+              Instagram
+            </a>
+          </p>
+          <p className="social-media">
+            <CiLinkedin className="text-2xl" />
+            <a target="_blank" href="https://www.linkedin.com/">
+              LinkedIn
+            </a>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SocialMedia;
---
+++ src/pages/ContactPage/ContactLocation.tsx
@@ -0,0 +1,15 @@
+const ContactLocation = () => {
+    return ( 
+            <section className="w-full mt-10">
+                <iframe
+                    className="h-[400px] w-full mx-auto"
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d29218.663153387362!2d90.3977075855197!3d23.735505571459257!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755b85c366afdaf%3A0x63cbcd8b4dfb9d3c!2z4Kau4Kak4Ka_4Kad4Ka_4KayLCDgpqLgpr7gppXgpr4!5e0!3m2!1sbn!2sbd!4v1726687319634!5m2!1sbn!2sbd"
+                    referrerPolicy="no-referrer-when-downgrade"
+                    style={{ border: 0 }} // Optional: to remove default iframe border
+                ></iframe>
+            </section>
+    
+    );
+};
+
+export default ContactLocation;
