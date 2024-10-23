dece9deb9e76c43aaff20db6f6bef8d3a23dfb66
Bilshansakib <burakaghani@gmail.com>
2024-10-24 05:08:45 +0600

    Update: OCRFeature- text area field added

--- src/dashboard/CompanyIncomes/InvoiceAndBilling/OCRFeature.tsx
+++ src/dashboard/CompanyIncomes/InvoiceAndBilling/OCRFeature.tsx
@@ -1,13 +1,23 @@
 import { Button, FormLabel } from "@mui/material";
+import { TextareaAutosize } from '@mui/base';
 
 const OCRFeature = () => {
   return (
-    <div className="shadow-xl rounded-lg p-6 bg-blue-100">
+    <div>
+      <div className="shadow-xl rounded-lg bg-blue-100  p-6 ">
       <h5>Give Your PDF Here</h5>
-      <div>
+      <div className="flex justify-center items-center">
         <FormLabel className="text-sm">Upload Image for OCR:</FormLabel>
-        <input type="file" accept="image/*" className="w-1/2 p-3 rounded  focus:border-red-400 focus:ring-red-300 focus:ring-opacity-40 dark:focus:border-red-300 focus:outline-none focus:ring" />
+        <input type="file" accept="image/*" className=" p-3 rounded  focus:border-red-400 focus:ring-red-300 focus:ring-opacity-40 dark:focus:border-red-300 focus:outline-none focus:ring" />
         <Button color="info" variant="contained">Extract Text</Button>
+      </div>
+      </div>
+      <div className="shadow-xl rounded-lg bg-blue-100  p-6 ">
+      
+      <div className="flex justify-start">
+        <FormLabel className="text-sm">Extracted Text:</FormLabel>
+        <TextareaAutosize></TextareaAutosize>
+      </div>
       </div>
      
     </div>
