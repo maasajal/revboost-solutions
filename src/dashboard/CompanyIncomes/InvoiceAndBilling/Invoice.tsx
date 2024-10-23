86fd01d885f7706bfcc977cbeefb2fbd93008842
Bilshansakib <burakaghani@gmail.com>
2024-10-23 14:47:21 +0600

    Change: animation and input placeHolder

--- src/dashboard/CompanyIncomes/InvoiceAndBilling/Invoice.tsx
+++ src/dashboard/CompanyIncomes/InvoiceAndBilling/Invoice.tsx
@@ -149,7 +149,7 @@ const Invoice = () => {
           >
             <div className="grid lg:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-4">
               <div className="space-y-4">
-                <div>
+                <div className="shadow-2xl rounded">
                   <label className="text-sm">Company Email</label>
                   <input
                     {...register("companyEmail", { required: true })}
@@ -158,7 +158,7 @@ const Invoice = () => {
                     className="w-full p-3 rounded  focus:border-red-400 focus:ring-red-300 focus:ring-opacity-40 dark:focus:border-red-300 focus:outline-none focus:ring"
                   />
                 </div>
-                <div>
+                <div className="shadow-2xl rounded">
                   <label className="text-sm">Customer Name</label>
                   <input
                     {...register("customerName", { required: true })}
@@ -167,50 +167,51 @@ const Invoice = () => {
                     className="w-full p-3 rounded  focus:border-red-400 focus:ring-red-300 focus:ring-opacity-40 dark:focus:border-red-300 focus:outline-none focus:ring"
                   />
                 </div>
-                <div>
+                <div className="shadow-2xl rounded">
                   <label className="text-sm">Your Company Name</label>
                   <input
                     {...register("companyName", { required: true })}
                     id="companyName"
                     type="text"
+                    placeholder="Company name"
                     className="w-full p-3 rounded focus:border-red-400 focus:ring-red-300 focus:ring-opacity-40 dark:focus:border-red-300 focus:outline-none focus:ring"
                   />
                 </div>
               </div>
               <div className="space-y-4">
-                <div>
+                <div className="shadow-2xl rounded">
                   <label className="text-sm">Invoice Number</label>
                   <input
                     {...register("invoiceNumber", { required: true })}
                     id="invoiceNumber"
                     type="number"
-                    placeholder=""
+                    placeholder="Invoice maunally"
                     className="w-full p-3 rounded  focus:border-red-400 focus:ring-red-300 focus:ring-opacity-40 dark:focus:border-red-300 focus:outline-none focus:ring"
                   />
                 </div>
-                <div>
+                <div  className="shadow-2xl rounded">
                   <label className="text-sm">Invoice Creation Date</label>
                   <input
                     {...register("date")}
                     id="invoiceCreationDate"
-                    // type="date"
-                    placeholder={getDate()}
+                    type="date"
+                   
                     className="w-full p-3 rounded  focus:border-red-400 focus:ring-red-300 focus:ring-opacity-40 dark:focus:border-red-300 focus:outline-none focus:ring"
                   />
                 </div>
               </div>
               <div className="space-y-4">
-                <div>
+                <div  className="shadow-2xl rounded">
                   <label className="text-sm">Due Date</label>
                   <input
                     {...register("invoiceDueDate", { required: true })}
                     id="InvoiceDueDate"
                     type="text"
-                    placeholder=""
+                    placeholder={getDate()}
                     className="w-full p-3 rounded  focus:border-red-400 focus:ring-red-300 focus:ring-opacity-40 dark:focus:border-red-300 focus:outline-none focus:ring"
                   />
                 </div>
-                <div>
+                <div className="shadow-2xl">
                   <label className="text-sm">Customer Address</label>
                   <input
                     {...register("customerAddress", { required: true })}
@@ -275,7 +276,7 @@ const Invoice = () => {
                     )}
                   />
                   <button
-                    className="w-full mt-4 p-3 rounded  bg-red-300 focus:ring-red-300 focus:ring-opacity-40 dark:focus:border-red-300 "
+                    className="w-full  mt-4 p-3 rounded  bg-red-300 focus:ring-red-300 focus:ring-opacity-40 dark:focus:border-red-300 "
                     type="button"
                     onClick={() => remove(index)}
                   >
@@ -301,7 +302,7 @@ const Invoice = () => {
                 </button>
                 <button
                   type="submit"
-                  className={loading === true ? `animate-spin w-full p-3 text-sm font-bold tracking-wide uppercase rounded dark:bg-red-400 dark:text-gray-50`:  `w-full p-3 text-sm font-bold tracking-wide uppercase rounded dark:bg-blue-400 dark:text-gray-50`}
+                  className={loading === true ? `animate-bounce w-full p-3 text-sm font-bold tracking-wide uppercase rounded dark:bg-red-400 dark:text-gray-50`:  `w-full p-3 text-sm font-bold tracking-wide uppercase rounded dark:bg-blue-400 dark:text-gray-50`}
                 >
                   {loading ? "Saving..." : "Save Invoice"}
                 </button>
