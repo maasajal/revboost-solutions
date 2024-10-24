6cf3fb8b1c9660c96b910b2b5e982592eac720c0
Abdullah Sajal <59488317+sajal-se@users.noreply.github.com>
2024-10-24 22:30:22 +0300

    Error: unused variable remove from Home component

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,7 +1,7 @@
 import Banner from "./HomeComponent/Banner";
 import FAQ from "./HomeComponent/FAQ";
 import Sectors from "./HomeComponent/Sectors";
-import Testimonials from "./HomeComponent/Testimonials";
+// import Testimonials from "./HomeComponent/Testimonials";
 import UnlockBusiness from "./HomeComponent/UnlockBusiness";
 import { Helmet } from "react-helmet";
 import UserFeedback from "./HomeComponent/UserFeedback";
